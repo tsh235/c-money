aba7d6e845a7122eda94dc3e8738c6a719217c71
tsh235 <tanyashustrova@yandex.ru>
2024-03-14 10:37:27 +0300

    fix bag

--- src/modules/handlers/handlePageExchange.js
+++ src/modules/handlers/handlePageExchange.js
@@ -1,3 +1,4 @@
+import { MY_ACCOUNT } from '../../index.js';
 import { renderExchange } from '../render/renderExchange.js';
 import { getData } from '../utils/getData.js';
 import { preload } from '../utils/preload.js';
